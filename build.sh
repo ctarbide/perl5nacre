0bcc5402bf45c2468ce51c7911441c1a4963456d
C. Tarbide <ctarbide@tuta.io>
2022-02-09 20:28:56 +0000

    fix Errno_pm.PL when dealing with newer gcc preprocessor output, handle both the old and newer output format using a simple state machine

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 set -eu #x
-die(){ ev=$1; shift; for msg in "$@"; do echo "${msg}"; done; exit "${ev}"; }
+die(){ ev=$1; shift; for msg; do echo "${msg}"; done; exit "${ev}"; }
 prefix=${HOME}/local
-make=make
+make=${MAKE:-make}
 test -x Configure || die 1 "ERROR: Wrong directory, 'cd' to the project root first."
 mkdir -p build
 cd build
 if [ ! -f config.h ]; then
-    ../Configure -Dmksymlinks -de -Dprefix="${prefix}"
+    ../Configure -des -Dmksymlinks \
+        -Dprefix="${prefix}" \
+        -Dcc=gcc
 fi
 if [ ! -f "${prefix}/bin/perl" -o ! -f x2p/a2p ]; then
-    ${make}
-    ${make} test
-    ${make} install
+    ${make} all 2>&1 | tee ../make-all.log
+    ${make} test 2>&1 | tee ../make-test.log
+    ${make} install 2>&1 | tee ../make-install.log
 fi
 echo "all done for ${0##*/}"
